5f1f8f3909f103d171316b31bc3fff6ae7e8ebab
lidiya <messsking@gmail.com>
2018-09-17 16:30:45 +0800

    updat e file

--- init-kvm.sh
+++ init-kvm.sh
@@ -2,6 +2,13 @@
 
 # This scripts is used to record the operation on a new kvm
 
-yum install -y git wget net-tool curl vim 
+# yum install -y git wget net-tool curl vim 
 
-wget http://appcenter-docs.qingcloud.com/developer-guide/scripts/app-agent-linux-amd64.tar.gz
+# wget http://appcenter-docs.qingcloud.com/developer-guide/scripts/app-agent-linux-amd64.tar.gz
+# tar -xvf app-agent-linux-amd64.tar.gz
+# bash app-agent-linux-amd64/install.sh
+
+# python ./scripts/get-pip.py
+# curl -fsSL https://get.docker.com/ | sh
+# systemctl start docker
+# bash ./scripts/get-image.sh
