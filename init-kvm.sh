1c15d7b0e732aad65f326c3df7e7791164fa9c72
lidiya <messsking@gmail.com>
2018-09-18 10:24:54 +0800

    add epel release

--- init-kvm.sh
+++ init-kvm.sh
@@ -2,7 +2,10 @@
 
 # This scripts is used to record the operation on a new kvm
 
-# yum install -y git wget net-tool curl vim 
+# yum install epel-release -y
+# yum install jq -y
+# jq --version
+# yum install -y git wget net-tool curl vim
 
 # wget http://appcenter-docs.qingcloud.com/developer-guide/scripts/app-agent-linux-amd64.tar.gz
 # tar -xvf app-agent-linux-amd64.tar.gz
